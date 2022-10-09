97c72922bc7bdf630ea5e1fc6868bdea4be6a34b
maityamit <maityamit308@gmail.com>
2022-10-09 20:44:05 +0530

    image import from gallery

--- app/src/main/java/com/example/faceapp/MainActivity.java
+++ app/src/main/java/com/example/faceapp/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.faceapp;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.faceapp.deepHelper.deepGraphicOverlay;
@@ -30,6 +34,7 @@ import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+import java.io.IOException;
 import java.util.List;
 
 import dmax.dialog.SpotsDialog;
@@ -38,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
     CameraView cameraView;
     deepGraphicOverlay deepgraphicOverlay;
     Button btn;
-    ImageView camera_face_change;
+    private final int GALLERY_INTENT_CODE = 993;
+    ImageView camera_face_change,photo_import_from_gallery,gallery_photo_show;
     AlertDialog waitingdialog;
     @Override
     protected void onResume() {
@@ -57,12 +63,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraView=findViewById(R.id.camera);
         camera_face_change = findViewById(R.id.camera_face_change);
+        gallery_photo_show = findViewById(R.id.gallery_photo_show);
+        photo_import_from_gallery=findViewById(R.id.photo_import_from_gallery);
         deepgraphicOverlay=findViewById(R.id.graphic_overlay);
         btn=findViewById(R.id.open);
         waitingdialog=new SpotsDialog.Builder().setContext(this)
                 .setMessage("please wait")
                 .setCancelable(false)
                 .build();
+
+        photo_import_from_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(
+                        Intent.createChooser(
+                                intent,
+                                "Select Image from here..."),
+                        GALLERY_INTENT_CODE);
+            }
+        });
 
         camera_face_change.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +97,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                gallery_photo_show.setVisibility(View.GONE);
+                cameraView.setVisibility(View.VISIBLE);
                 cameraView.start();
                 cameraView.captureImage();
                 deepgraphicOverlay.clear();
@@ -150,5 +174,32 @@ public class MainActivity extends AppCompatActivity {
         builder.setMessage(String.format("Detected %d faces in image",count));
         builder.show();
     }
+
+    //Photo Import from gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            Uri uri = (Uri) data.getData();
+            // Image received from gallery
+            if (requestCode == GALLERY_INTENT_CODE) {
+                try {
+                    // Converting the image uri to bitmap
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                    waitingdialog.show();
+                    bitmap=Bitmap.createScaledBitmap(bitmap,cameraView.getWidth(),cameraView.getHeight(),false);
+                    gallery_photo_show.setVisibility(View.VISIBLE);
+                    cameraView.setVisibility(View.GONE);
+                    gallery_photo_show.setImageBitmap(bitmap);
+                    runFaceDetector(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 }
